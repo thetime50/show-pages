96710390199afec9f2a748336a082751978f668c
thetime50 <1004763777@qq.com>
2019-04-03 01:31:29 +0800

    添加onload事件开始游戏 隐藏按钮显示

--- t-rex-runner-zero/components/neterror/resources/loadTimeData.js
+++ t-rex-runner-zero/components/neterror/resources/loadTimeData.js
@@ -1,20 +1,33 @@
 var loadTimeData={
-	values:{
-	  offlineContentList:"",
-	  //downloadButton
-	  //suggestedOfflineContentPresentation
-	  //attemptAutoFetch
-	  //reloadButton.msg
-	  // Check for Google cached copy suggestion.
-	  //cacheButton
-	  //disabledEasterEgg
-	},
-	getValue:function (s){
-	  return this.values[s]
-	},
-	valueExists:function (s){
-	  return this.values.hasOwnProperty(s)
-	}
- }
+  values:{
+    offlineContentList:"",
+    //downloadButton
+    //suggestedOfflineContentPresentation
+    //attemptAutoFetch
+    //reloadButton.msg
+    // Check for Google cached copy suggestion.
+    //cacheButton
+    //disabledEasterEgg
+  },
+  getValue:function (s){
+    return this.values[s]
+  },
+  valueExists:function (s){
+    return this.values.hasOwnProperty(s)
+  }
+}
 
 //var iconClass='icon-offline'
+
+
+function addWindowEvent(event, fun, useCapture){
+	if(window.attachEvent){
+		window.attachEvent('on'+event, fun)
+	}else if(window.addEventListener){
+		window.addEventListener(event, fun, useCapture)
+	}
+}
+addWindowEvent("load",function (){
+  console.log("hello")
+  new Runner('.interstitial-wrapper')
+})
